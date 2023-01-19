62bc977630249cd432bf3e7764327d98a631ec28
VikasAmbekar <ambekarviks@gmail.com>
2023-01-19 12:15:18 +0530

    Axis Saral Spring Boot

---
+++ Axis-Saral-Admin/src/main/java/com/axis/AxisSaralAdminApplication.java
@@ -0,0 +1,20 @@
+package com.axis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AxisSaralAdminApplication {
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {return new RestTemplate();}
+	public static void main(String[] args) {
+		SpringApplication.run(AxisSaralAdminApplication.class, args);
+	}
+
+}
---
+++ Axis-Saral-Admin/src/main/java/com/axis/entity/Admin.java
@@ -0,0 +1,66 @@
+package com.axis.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+    private String adminname;
+    private String adminemailId;
+    private String adminPassword;
+    @Id
+    private long mobileNo;
+
+    public Admin() {
+    }
+
+    public Admin(String adminname, String adminemailId, String adminPassword, long mobileNo) {
+        this.adminname = adminname;
+        this.adminemailId = adminemailId;
+        this.adminPassword = adminPassword;
+        this.mobileNo = mobileNo;
+    }
+
+    public String getAdminname() {
+        return adminname;
+    }
+
+    public void setAdminname(String adminname) {
+        this.adminname = adminname;
+    }
+
+    public String getAdminemailId() {
+        return adminemailId;
+    }
+
+    public void setAdminemailId(String adminemailId) {
+        this.adminemailId = adminemailId;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public long getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(long mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "adminname='" + adminname + '\'' +
+                ", adminemailId='" + adminemailId + '\'' +
+                ", adminPassword='" + adminPassword + '\'' +
+                ", mobileNo=" + mobileNo +
+                '}';
+    }
+
+}
