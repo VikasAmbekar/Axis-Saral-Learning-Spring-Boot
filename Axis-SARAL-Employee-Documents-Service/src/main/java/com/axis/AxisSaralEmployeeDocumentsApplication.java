8165f623792ec30a9bd4f9f36ef6fd7b463db111
VikasAmbekar <ambekarviks@gmail.com>
2023-01-19 18:48:35 +0530

    Axis Saral Spring Boot

---
+++ Axis-SARAL-Employee-Documents-Service/src/main/java/com/axis/AxisSaralEmployeeDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.axis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisSaralEmployeeDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxisSaralEmployeeDocumentsApplication.class, args);
+	}
+
+}
---
+++ Axis-SARAL-Employee-Documents-Service/src/test/java/com/axis/AxisSaralEmployeeDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxisSaralEmployeeDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
